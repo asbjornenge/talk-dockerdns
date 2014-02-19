ed512e2962adc16f1f699cf5f1e1d330fa6fccf0
Asbjørn Enge <asbjorn@hanafjedle.net>
2014-02-19 08:12:26 +0100

    Improved gulp strucuter

--- gulpfile.js
+++ gulpfile.js
@@ -2,44 +2,30 @@ var gulp = require('gulp'),
     lr = require('tiny-lr'),
     server = lr();
 
-var EXPRESS_PORT = 4000;
-var EXPRESS_ROOT = 'assets';
+var SERVER_PORT = 4000;
+var SERVER_ROOT = 'assets';
 var LIVERELOAD_PORT = 35729;
 
 function startExpress() {
-  var express = require('express');
-  var app = express();
-//  app.use(require('connect-livereload')());
-  app.use(express.static(EXPRESS_ROOT));
-  app.listen(EXPRESS_PORT);
-  console.log('Development server running on port '+EXPRESS_PORT);
+
 }
 
-// Watch
+/** WATCH **/
+
 gulp.task('watch', function() {
-
-  // Listen on port 35729
-  server.listen(35729, function (err) {
-    if (err) {
-      return console.log(err)
-    };
-
-    // Watch .scss files
-    //gulp.watch('src/styles/**/*.scss', ['styles']);
-
-    // Watch .js files
-    //gulp.watch('src/scripts/**/*.js', ['scripts']);
-
-    // Watch image files
-    //gulp.watch('src/images/**/*', ['images']);
-
-  });
-
+    server.listen(LIVERELOAD_PORT, function (err) {
+        if (err) { return console.log(err) };
+    });
 });
 
-// Dev server
+/** SERVER **/
 
-gulp.task('dev', function() {
-	startExpress();
-  gulp.watch();
+gulp.task('server', function() {
+    var express = require('express');
+    var app = express();
+    //  app.use(require('connect-livereload')());
+    app.use(express.static(SERVER_ROOT));
+    app.listen(SERVER_PORT);
+    console.log('Development server running on port '+SERVER_PORT);
+    // gulp.watch();
 })
