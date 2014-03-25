46a974c2d3f4cb2afa1becac6b1bd4ef06bbffc6
Asbjørn Enge <asbjorn@hanafjedle.net>
2014-03-25 13:03:32 +0100

    Implemented navigation

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,1 @@
-console.log("eple");
+""===window.location.hash&&(window.location.hash="#1");var p=function(){};p.prototype.replace_hash=function(a){0>a||(window.location.hash="#"+a)},p.prototype.navigate=function(a){switch(a){case"right":this.replace_hash(parseInt(window.location.hash.split("#")[1])+1);break;case"left":this.replace_hash(parseInt(window.location.hash.split("#")[1])-1)}};var presentation=new p;document.body.onkeydown=function(a){switch(a.which){case 39:presentation.navigate("right");break;case 37:presentation.navigate("left")}};
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,31 @@
-console.log('eple')
+
+if (window.location.hash === "") window.location.hash = "#1";
+
+var p = function() {}
+p.prototype.replace_hash = function(page) {
+    if (page < 0) return
+    window.location.hash = "#"+page
+}
+p.prototype.navigate = function(to) {
+    switch(to) {
+        case 'right':
+            this.replace_hash(parseInt(window.location.hash.split('#')[1])+1)
+            break;
+        case 'left':
+            this.replace_hash(parseInt(window.location.hash.split('#')[1])-1)
+            break;
+    }
+};
+
+var presentation = new p();
+
+document.body.onkeydown = function(e) {
+    switch(e.which) {
+        case 39:
+            presentation.navigate('right')
+            break;
+        case 37:
+            presentation.navigate('left')
+            break;
+    }
+}
